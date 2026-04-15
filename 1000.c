d8da3d66f2738545509b5f89bb5ffd5250efaeaf
Davi Parada <davi.parada@unifesp.br>
2026-04-14 22:31:01 -0300

    Exercício 1000 do BeeCrowd
    Added header comments and main function to print 'Hello World'.

--- 1000.c
+++ 1000.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Davi de Oliveira Parada
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 14/04/2026
+Objetivo    : Escrever Hello World
+Aprendizado : Utilização da função printf
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main() {
