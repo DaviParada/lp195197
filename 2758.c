8b93846fb40261f22c7884d76a4fb81d3776e4e3
Davi Parada <davi.parada@unifesp.br>
2026-04-14 22:36:59 -0300

    Exercício 2758 do BeeCrowd
    Added header comments with exercise details.

--- 2758.c
+++ 2758.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Davi de Oliveira Parada
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 2758
+Data        : 09/04/2026
+Objetivo    : Manipular números com casas decimais diferentes
+Aprendizado : Utilização de recursos do %
+-------------------------------------------------------------------------- */
 #include <stdio.h>
  
 int main() {
