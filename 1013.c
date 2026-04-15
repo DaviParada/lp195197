d014ed9f590f804b62a7f9954d184f0454a95d64
Davi Parada <davi.parada@unifesp.br>
2026-04-14 22:33:32 -0300

    Exercício 1013 BeeCrowd

--- 1013.c
+++ 1013.c
@@ -1,3 +1,15 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Davi de Oliveira Parada
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1013
+Data        : 07/04/2026
+Objetivo    : Exibir o maior valor entre dois valores
+Aprendizado : Utilização do scanf e tipos de variáveis
+-------------------------------------------------------------------------- */
+
+
 #include <stdio.h>
  
 int main() {
