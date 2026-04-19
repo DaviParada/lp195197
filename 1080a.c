39c1b9ae692a110690e214e5960dbb34e8843a97
Davi Parada <davi.parada@unifesp.br>
2026-04-19 15:06:02 -0300

    Exercício 1080 do BeeCrowd utilizando Array

--- 1080a.c
+++ 1080a.c
@@ -1,3 +1,16 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Davi de Oliveira Parada
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 2757
+Data        : 19/04/2026
+Objetivo    : Maior entre 100 números
+Aprendizado : Utilização de Array
+-------------------------------------------------------------------------- */
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
  
